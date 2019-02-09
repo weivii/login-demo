b9ef6ffc7cbaa0cf86c8d64898576f19ad7f3e3c
wwang <wangwei@qq.com>
2019-02-09 14:39:46 +0800

    finish

--- src/main.js
+++ src/main.js
@@ -5,9 +5,55 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './style/common.css'
 
-
 Vue.use(ElementUI);
+//导入axios
+import axios from 'axios'
+//设置地址公共部分.
+axios.defaults.baseURL = 'http://litc.pro:9999/v1';
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  // 发送请求前带上token就不用每次发送请求都手动带上headers{"Authorization":token}
+ let token = localStorage.getItem("token") 
+ if(token){
+   config.headers.Authorization = token
+ }
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
 
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
+
+//将axios以$http为名作为属性挂载在vue原型上，后面用到axios的地方直接使用$http
+Vue.prototype.$http=axios
+
+// 路由导航守卫 作用：在每一次路由跳转的时候，都会触发一系列的回调函数，这些回调函数成为导航守卫，可以在这些会调函数中进行路由导航拦截操作
+// 在进入某个路由前
+router.beforeEach((to, from, next) => {
+  
+// 如添加了导航守卫的会调函数，必须调用next函数，将其引导到某个页面，如果不传参就是不干预路由跳转
+
+let token=localStorage.getItem("token")
+if(!token&&to.path!=="/login"){
+// 判断如果不加上to.path!=="/login"会陷入死递归，会一直跳转login页面没有出口
+  return next("/login")
+}
+ next()
+})
+// 在进入之后
+router.afterEach((to, from) => {
+ 
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -6,10 +6,10 @@
             <el-input v-model="username"></el-input>
         </el-form-item>
         <el-form-item label="密码">
-            <el-input type="password" v-model="password"></el-input>
+            <el-input type="password" v-model="password" @keydown.native.enter="login"></el-input>
         </el-form-item>
         <el-form-item class="botton">
-         <el-button type="primary" @click="login">登录</el-button>
+         <el-button type="primary" @click="login" >登录</el-button>
         </el-form-item>
         </el-form>
     </div>
@@ -20,12 +20,43 @@ export default {
       return {
         labelPosition: 'right',
         username:"",
-        password:"",
+        password:""
       };
     },
     methods:{
+        
         login(){
 
+            if(!this.username.trim()||!this.password.trim()) 
+            return   this.$message({
+            message: '请输入用户名或密码',
+            type: 'warning'
+            });
+            //发送axios请求post，地址，参数，结果回调函数
+            this.$http.post('/users/login', {
+            username: this.username,
+            password: this.password
+            })
+            .then(result=> {
+                console.log(result.data)
+                // console.log(result.data.data.token)
+                //登陆成功将获取的token 保存到本地
+                let token = localStorage.setItem("token",result.data.data.token);
+                let  userInfo = localStorage.setItem("userInfo",JSON.stringify(result.data.data));
+                this.$message({
+                message:result.data.succMsg,
+                type: 'success'
+                });
+                //登陆成功跳转到首页
+                this.$router.push("/home");
+            })
+            //.then的回调函数有两种，一个是成功的，第二个是失败的
+            //但是如果失败了也会报错，通过.catch也可以进行捕获
+            .catch((err)=> {
+                console.dir(err)
+                console.log(err.response.data.errMsg)
+                this.$message.error('错了哦!'+err.response.data.errMsg);
+            });
         }
     }
 }
